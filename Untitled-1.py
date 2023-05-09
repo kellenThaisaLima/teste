05ea0b6cd8f22fdf11ec34763ecec71aee8fe420
Kellen <kellen1393@gmail.com>
2023-05-08 21:35:32 -0300

    teste

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+cores = "rosa", "verde", "azul"
+
+# lista = list(cores)
+
+lista = list(cores)
+
+for cor in cores: 
+    lista.append(cor + "s")
+
+print(lista)
+
+# cor in cores:print(list)
+
+# print(lista)
+    
